e61d41eb756b7c16d3db298cfb177ee985258889
joster1697 <jorgemendoza1697@gmail.com>
2025-05-26 22:39:15 -0600

    se automatiza creación de tablas

--- server/config/config.js
+++ server/config/config.js
@@ -1,23 +1,25 @@
+require('dotenv').config();
+
 module.exports = {
   development: {
-    username: `${process.env.DB_USERNAME}`,
+    username: `${process.env.DB_USER}`,
+    password: `${process.env.DB_PASSWORD}`,
+    database: `${process.env.DB_NAME}`,
+    host: `localhost`,
+    dialect: `mysql`,
+  },
+  test: {
+    username: `${process.env.DB_USER}`,
     password: `${process.env.DB_PASSWORD}`,
     database: `${process.env.DB_NAME}`,
     host: `${process.env.DB_HOST}`,
     dialect: `mysql`,
   },
-  test: {
-    username: `${process.env.TEST_DB_USERNAME}`,
-    password: `${process.env.TEST_DB_PASSWORD}`,
-    database: `${process.env.TEST_DB_NAME}`,
-    host: `${process.env.TEST_DB_HOST}`,
-    dialect: `${process.env.TEST_DB_DIALECT}`,
-  },
   production: {
-    username: `${process.env.PROD_DB_USERNAME}`,
-    password: `${process.env.PROD_DB_PASSWORD}`,
-    database: `${process.env.PROD_DB_NAME}`,
-    host: `${process.env.PROD_DB_HOST}`,
-    dialect: `${process.env.PROD_DB_DIALECT}`,
+    username: `${process.env.DB_USER}`,
+    password: `${process.env.DB_PASSWORD}`,
+    database: `${process.env.DB_NAME}`,
+    host: `${process.env.DB_HOST}`,
+    dialect: `mysql`,
   },
 };
