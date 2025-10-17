a1e3155392424642c9f2b5d81e2e22e073df8e4d
BrandonVLA <bbarrantescalderon@vlacademy.com>
2025-10-17 16:02:45 -0600

    Se agrego useNavigate a login para que botones funcionaran entre componentes login y register

--- client/src/features/auth/components/login.jsx
+++ client/src/features/auth/components/login.jsx
@@ -13,6 +13,8 @@ export default function Login() {
     });
   };
 
+  const navigate = useNavigate();
+
   const handleLogin = async () => {
     e.preventDefault();
     if (handleLogin()) {
@@ -89,10 +91,11 @@ export default function Login() {
                 Login
               </button>
               <button
-                type="submit"
+                type="button"
                 className="mt-4 w-full text-white bg-green-700 hover:bg-green-800 font-medium rounded-lg text-sm px-5 py-2.5"
+                onClick={() => navigate("/register")}
               >
-                Create Account
+                New here? <span className="font-bold"><em>Register</em></span>
               </button>
             </div>
           </div>
--- client/src/features/auth/components/register.jsx
+++ client/src/features/auth/components/register.jsx
@@ -126,10 +126,14 @@ export default function Register() {
                 Create Account
               </button>
               <button
-                type="submit"
+                type="button"
                 className="mt-4 w-full text-white bg-green-700 hover:bg-green-800 font-medium rounded-lg text-sm px-5 py-2.5"
+                onClick={() => navigate("/login")}
               >
-                Already have an account? Login
+                Already have an account?{" "}
+                <span className="font-bold">
+                  <em>Login</em>
+                </span>
               </button>
             </div>
           </form>
