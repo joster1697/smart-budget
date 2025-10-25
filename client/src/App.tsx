06346b742f7ea9e2618a97b45d7549fcfadb90ac
BrandonVLA <bbarrantescalderon@vlacademy.com>
2025-10-25 00:27:31 -0600

    AddTransactionBtn added to UI components

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -5,12 +5,14 @@ import Login from "./pages/Login";
 import Sidebar from "./components/menus/Sidebar.jsx";
 import Footer from "./components/menus/Footer.jsx";
 import Searchbar from "./components/menus/Searchbar.jsx";
+import AddTransactionBtn from "./components/ui/buttons/AddTransactionBtn.jsx"
 
 function App() {
   return (
     <section className="app-layout">
       {/* <Sidebar /> */}
-      <Searchbar/>
+      {/* <Searchbar/> */}
+      <AddTransactionBtn/>
       {/* <Footer/> */}
       <main>
         <Routes>
